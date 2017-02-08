8cca52b7b93fccb6f089b2c4be22105f6b82ca09
anthonysherwinchua <anthonysherwinchua@gmail.com>
2017-02-08 23:13:18 +0800

    update homepage

--- app/service/scrape_instagram_service.rb
+++ app/service/scrape_instagram_service.rb
@@ -3,12 +3,19 @@ class ScrapeInstagramService
   def self.call
     last_image = Image.order(updated_at: :desc).first
 
-    items = InstaScrape.hashtag(ENV['INSTAGRAM_TAG'])
-    items.each do |item|
-      Image.create(image_url: item.image)
+    hashtags = ENV['INSTAGRAM_TAG'].split(',')
+
+    items = []
+    hashtags.each do |hashtag|
+      items << InstaScrape.hashtag(hashtag).map(&:image)
     end
 
-    Image.where('updated_at <= ?', last_image.updated_at).destroy_all
+    items = items.flatten.uniq
+    items.each do |item|
+      Image.create(image_url: item)
+    end
+
+    Image.where('updated_at <= ?', last_image.updated_at).destroy_all if last_image
   end
 
 end
